e7db51656b1388f1f9e33e03da274f6704148e10
Chimba <chimbeevaa@gmail.com>
2022-09-11 21:06:44 +0800

    11092022

---
+++ src/TodoList.js
@@ -0,0 +1,5 @@
+export default function TodoList(props) { // ...................... 1
+    return (
+        <section> // .................................................. 2
+            <h1>Дела</h1>
+            <table className="table is-hoverable is-fullwidth"> // ...... 3
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import 'bulma/css/bulma.css'; //поменяли таблицу стилей. Используем css framework Bulma
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
